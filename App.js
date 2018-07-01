48507a4629a4bacdf7c7968700a084eed5e78b71
PAULO OLIVEIRA <paulosoujava@gmail.com>
2018-06-30 21:13:02 -0300

    First commit

--- App.js
+++ App.js
@@ -1,115 +1,136 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  Image,
-  View
-} from 'react-native';
+import { View, StyleSheet,Platform, ImageBackground, Text, Button,Image } from 'react-native';
 
-export default class App extends Component {
-  constructor(props){
+export default class PrimeiroProjeto extends Component {
+
+  constructor(props) {
     super(props);
-    this.state = {titleMimi: "", footerMimi: ''}
+    this.state = {consumido:0, status:'Ruim', pct:0, foto:'https://cdn4.iconfinder.com/data/icons/reaction/32/angry-512.png'};
 
-    this.writeMimimi = this.writeMimimi.bind(this);
+    this.addCopo = this.addCopo.bind(this);
+    this.atualizar = this.atualizar.bind(this);
+    
   }
 
-  changeToMimimi(mimimi){
-    let newText = mimimi.toLowerCase();
+  atualizar() {
+    let s = this.state;
+    s.pct = Math.floor((s.consumido/2000)*100);
 
-    return newText.replace(/(a|e|i|o|u|á|á|á|á|ú|ã|õ|à|è|ì|â|ô)/g, 'i');
-    
-}
-  
-    writeMimimi(w){
-      let s = this.state;
-      s.titleMimi = w;
-      s.footerMimi = this.changeToMimimi(w);
-      this.setState(s);
+    if(s.pct >= 100) {
+      s.status = "Bom";
+    } else {
+      s.status = "Ruim";
     }
-  
+    if(s.pct >= 50 && s.pct <= 99 ) {
+    
+        s.foto = 'https://pt.seaicons.com/wp-content/uploads/2016/04/Smiley-scared-icon.png';
+    }  
+      if(s.pct == 100 ) {
+    
+        s.foto = 'https://vignette.wikia.nocookie.net/robocraft/images/2/26/Smile.png/revision/latest?cb=20150110152445';
+    }
+
+    this.setState(s);
+  }
+
+  addCopo() {
+    let s = this.state;
+    s.consumido += 200;
+    this.setState(s);
+
+    this.atualizar();
+  }
+
   render() {
     return (
-     <View style={styles.body}>
-       <View>
-          <Text style={styles.title}>Create Mimimi</Text>    
-       </View>
-       <View style={styles.inputArea}>
-         <TextInput style={styles.input}  onChangeText={this.writeMimimi} placeholder="Write something..."/>    
-       </View>
+      <View style={styles.body}>
+        <ImageBackground source={require('./image/waterbg.png')} style={styles.bgimage}>
 
-       <View style={styles.area}>
-         <Text style={[styles.title_all, styles.title_mimimi]}>
-           {this.state.titleMimi.toUpperCase()}
-         </Text>    
-         <Image style={styles.image_mimimi} 
-            source={{uri:'https://conteudo.imguol.com.br/c/noticias/53/2016/08/29/bebe-chorando-choro-mimimi-1472504477218_956x500.jpg'}} />
-          <Text style={[styles.title_all, styles.footer_mimimi]}>
-            {this.state.footerMimi.toUpperCase()}
-          </Text>    
-       </View>
-     </View>
+            <View style={styles.infoArea}>
+             
+            
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Meta</Text>
+                <Text style={styles.areaDado}>2000ml</Text>
+              </View>
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Consumido</Text>
+                <Text style={styles.areaDado}>{this.state.consumido}ml</Text>
+              </View>
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Status</Text>
+                <Text style={styles.areaDado}>{this.state.status}</Text>
+               </View>
+                <Image
+               style={{width: 50, height: 50, marginRight:5}}
+                source={{uri: this.state.foto} }
+              />
+            </View>
+            <View style={styles.content}>
+              <Text style={styles.pctText}>{this.state.pct}%</Text>
+              <Button title="Beber 200ml" onPress={this.addCopo} 
+              color={Platform.OS === 'ios' ? color="#FFF" : null }/>
+          </View>
+
+        </ImageBackground>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   body:{
-    backgroundColor:'#999',
-    paddingTop:30,
+    flex:1
+  },
+  bgimage:{
     flex:1,
-    flexDirection:'column',
+    width:null
+  },
+  infoArea:{
+    flex:1,
+    flexDirection:'row',
+    paddingTop:45,
+
+  },
+  area:{
+    flex:1,
     alignItems:'center'
   },
-  title:{
-    fontSize:30,
-    color:'#FFF'
-  }, 
-  inputArea:{
-    alignSelf:'stretch'
+  areaTitulo:{
+    color:'#45b2fc'
   },
-  input:{
-    borderWidth:1,
-    borderColor: '#999',
-    backgroundColor: '#EEE',
-    color:'#000',
-    height:40,
-    margin:20,
-    padding:10
+  areaDado:{
+    color:'#2b4274',
+    fontSize:15,
+    fontWeight:'bold'
   },
-  image_mimimi:{
-    height:400,
-    width:400,
-    marginTop:-90,
-    zIndex:0
+  content:{
+  flex:2,
+  backgroundColor:'transparent',
+  alignItems:'center'
   },
-  title_all:{
-  fontSize:30,
-    color:'#FFF',
-    padding:10,
-    backgroundColor: 'transparent',
-    fontWeight:'bold',
-    textAlign:'left',
-    marginLeft:10,
-    height:70
+  pctText:{
+    fontSize:70,
+    marginTop:90,
+    color:'#FFFFFF',
+    backgroundColor:'transparent'
   },
-  title_mimimi:{
-    marginTop:20,
-    zIndex:1
-    
-  },
-  footer_mimimi:{
-    marginTop:-60,
-    zIndex:1
+  btn:{
+    color: Platform.OS === 'ios' ? color="#FFF" : color="#eee" 
   }
-
-  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
